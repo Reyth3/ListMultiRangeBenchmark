d093e8473e5c5ed6f2ba210542eabca4489b74b7
Reyth3 <mrreynevan2@gmail.com>
2017-04-11 22:52:39 +0200

    Added String Generator

--- ListMultiRangeBenchmark/Program.cs
+++ ListMultiRangeBenchmark/Program.cs
@@ -8,8 +8,35 @@ namespace ListMultiRangeBenchmark
 {
     class Program
     {
+        static Random rand;
+        static string abc;
+        static string[] GenerateStrings(int count)
+        {
+            var strings = new List<string>();
+            for(int i = 0; i < count; i++)
+            {
+                var length = rand.Next(6, 13);
+                var s = "";
+                for (int j = 0; j < length; j++)
+                    s += abc[rand.Next(0, abc.Length)];
+                strings.Add(s);
+            }
+            return strings.ToArray();
+        }
+
         static void Main(string[] args)
         {
+            rand = new Random();
+            abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+            var ranges = new List<string[]>();
+            for (int i = 0; i < 500; i++)
+                ranges.Add(GenerateStrings(50000));
+
+            // Test A
+
+            // Test B
+
         }
     }
 }
